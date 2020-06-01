f33fd5d5b7b4df9774931ca2d7882c9587054b6d
Prabhat Ojha <prabhatojha90@gmail.com>
2020-06-01 23:49:52 +0530

    working on scaling with percentage

--- frontend/src/app/modules/builder/canvas/canvas-scale/canvas-scale.component.ts
+++ frontend/src/app/modules/builder/canvas/canvas-scale/canvas-scale.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-canvas-scale',
   templateUrl: './canvas-scale.component.html',
   styleUrls: ['./canvas-scale.component.scss']
 })
-export class CanvasScaleComponent implements OnInit {
+export class CanvasScaleComponent implements OnChanges {
+
+  @Input() node;
+  selectedScale = 100;
+  scaleOptions: {
+    75: .75,
+    100: 1,
+    125: 1.25,
+    150: 1.5,
+    200: 2,
+    300: 3
+  };
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    this.setIntialScale();
   }
 
+  setIntialScale() {
+    console.log(this.node.ko);
+  }
+
+  zoomIn() {
+    console.log(this.node);
+  }
+
+  changeScale(opt) {
+    this.node.style.cssText = 'transition: transform ease .3s;' +
+      `transform: scale(${this.scaleOptions[opt]});`;
+  }
+
+  zoomOut() {
+    this.node.style.transform = 'scale(1)';
+  }
 }
