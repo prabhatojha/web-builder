e6ffce1c6693dacd55f599e47685e2c8891d0771
arpit175 <arpit.ojha77@gmail.com>
2020-06-06 12:03:15 +0530

    project route added

---
+++ Model/project.js
@@ -0,0 +1,29 @@
+const mongoose =require('mongoose');
+const Schema=mongoose.Schema;
+
+const ProjectSchema= new Schema({
+currentZIndex:{
+    type:Number,
+     required:[true,'CurrentZIndex is required']
+    },
+name:{type:String,
+    required:[true,'Name is required']
+},
+id:{type:String,
+    required:[true,'Id is required']
+},
+pages: [{
+
+pageId:{type:String,
+},
+style:{type:Array
+},
+attribute:{type:Array
+},
+children:{type:Array
+}
+}]
+});
+
+const project=mongoose.model('project',ProjectSchema);
+module.exports=project;
---
+++ routes/project.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const Project=require('../Model/project');
+
+router.post('/', function(req, res, next) {
+Project.create(req.body).then(function(project){
+  res.send(project);
+});
+});
+
+router.delete('/',function(req,res,next){
+  Project.findByIdAndRemove({id:req.body.id}).then(function(project){
+req.send(project);
+  });
+});
+
+router.get('/',function(req,res,next){
+Project.find({}).then(function(projects){
+  res.send(projects);
+});
+
+});
+
+router.put('/',function(req,res,next){
+  Project.findByIdAndUpdate({_id:req.body._id},req.body).then(function(project){
+  res.send(project);
+  });
+});
+  
+module.exports = router;
