cf10c289382b8000eff201fa84cfafd581de51d9
Prabhat Ojha <prabhatojha90@gmail.com>
2020-07-04 15:58:50 +0530

    changed text behaviour

--- frontend/src/app/modules/builder/picker/text-picker/text-picker.service.ts
+++ frontend/src/app/modules/builder/picker/text-picker/text-picker.service.ts
@@ -9,55 +9,18 @@ import { CSS_PROPERTIES } from 'src/app/constants/css-constants';
 })
 export class TextPickerService {
   list: TextPickerModel[] = [];
-  customTextFonts = [
-    {
-      family: 'Sriracha',
-      url: 'http://fonts.gstatic.com/s/rajdhani/v9/LDIxapCSOBg7S-QT7q4AOeekWPrP.ttf',
-      fontSize: '35px',
-      innerText: 'Enter Text'
-    },
-    {
-      family: 'Metal Mania',
-      url: 'http://fonts.gstatic.com/s/metalmania/v9/RWmMoKWb4e8kqMfBUdPFJeXCg6UKDXlq.ttf',
-      fontSize: '30px',
-      innerText: 'Enter Text'
-    },
-    {
-      family: 'Piedra',
-      url: 'http://fonts.gstatic.com/s/piedra/v8/ke8kOg8aN0Bn7hTunEyHN_M3gA.ttf',
-      fontSize: '30px',
-      innerText: 'Enter Text'
-    },
-    {
-      family: 'Odibee Sans',
-      url: 'http://fonts.gstatic.com/s/odibeesans/v1/neIPzCSooYAho6WvjeToRYkyepH9qGsf.ttf',
-      fontSize: '30px',
-      innerText: 'Enter Text'
-    },
-    {
-      family: 'Staatliches',
-      url: 'http://fonts.gstatic.com/s/staatliches/v3/HI_OiY8KO6hCsQSoAPmtMbectJG9O9PS.ttf',
-      fontSize: '30px',
-      innerText: 'Enter Text'
-    },
-    {
-      family: 'Lexend Tera',
-      url: 'http://fonts.gstatic.com/s/lexendtera/v1/RrQUbo98_jt_IXnBPwCWtZhARYMgGtWA.ttf',
-      fontSize: '30px',
-      innerText: 'Enter Text'
-    }
-  ];
+
   constructor(private fontService: FontFamilyService) {
     this.init();
   }
 
   init() {
     // tslint:disable-next-line: prefer-for-of
-    // for (let i = 0; i < this.fontService.customTextFonts.length; i++) {
-    //   this.buildTextPickerItem(this.fontService.customTextFonts[i]);
-    // }
+    for (let i = 0; i < 2; i++) {
+      this.buildTextPickerItem(this.fontService.customTextFonts[i]);
+    }
 
-    this.buildImageTypeText();
+    // this.buildImageTypeText();
   }
 
   buildImageTypeText() {
@@ -76,7 +39,6 @@ export class TextPickerService {
     picker.innerText = item.innerText; // Picker element text
     picker.canvasElement.children[0].innerText = 'Double click to edit'; // Canvas element text
     picker.canvasElement.style[CSS_PROPERTIES.FONT_FAMILY] = item.family;
-
     this.list.push(picker);
 
   }
--- frontend/src/app/modules/shared/services/font/font-family.service.ts
+++ frontend/src/app/modules/shared/services/font/font-family.service.ts
@@ -10,41 +10,53 @@ export class FontFamilyService {
 
   customTextFonts = [
     {
-      family: 'Sriracha',
-      files: { regular: 'http://fonts.gstatic.com/s/rajdhani/v9/LDIxapCSOBg7S-QT7q4AOeekWPrP.ttf' },
-      fontSize: '35px',
-      innerText: 'Enter Text'
+      family: 'Josefin Sans',
+      files: { regular: 'http://fonts.gstatic.com/s/josefinsans/v16/Qw3PZQNVED7rKGKxtqIqX5E-AVSJrOCfjY46_DjQXMFrLgTsQV0.ttf' },
+      fontSize: '40px',
+      innerText: 'Add header'
     },
     {
-      family: 'Metal Mania',
-      files: { regular: 'http://fonts.gstatic.com/s/metalmania/v9/RWmMoKWb4e8kqMfBUdPFJeXCg6UKDXlq.ttf' },
-      fontSize: '30px',
-      innerText: 'Enter Text'
-    },
-    {
-      family: 'Piedra',
-      files: { regular: 'http://fonts.gstatic.com/s/piedra/v8/ke8kOg8aN0Bn7hTunEyHN_M3gA.ttf' },
-      fontSize: '30px',
-      innerText: 'Enter Text'
-    },
-    {
-      family: 'Odibee Sans',
-      files: { regular: 'http://fonts.gstatic.com/s/odibeesans/v1/neIPzCSooYAho6WvjeToRYkyepH9qGsf.ttf' },
-      fontSize: '30px',
-      innerText: 'Enter Text'
-    },
-    {
-      family: 'Staatliches',
-      files: { regular: 'http://fonts.gstatic.com/s/staatliches/v3/HI_OiY8KO6hCsQSoAPmtMbectJG9O9PS.ttf' },
-      fontSize: '30px',
-      innerText: 'Enter Text'
-    },
-    {
-      family: 'Lexend Tera',
-      files: { regular: 'http://fonts.gstatic.com/s/lexendtera/v1/RrQUbo98_jt_IXnBPwCWtZhARYMgGtWA.ttf' },
-      fontSize: '30px',
-      innerText: 'Enter Text'
+      family: 'Josefin Sans',
+      files: { regular: 'http://fonts.gstatic.com/s/josefinsans/v16/Qw3PZQNVED7rKGKxtqIqX5E-AVSJrOCfjY46_DjQXMFrLgTsQV0.ttf' },
+      fontSize: '24px',
+      innerText: 'Add sub header'
     }
+    // {
+    //   family: 'Sriracha',
+    //   files: { regular: 'http://fonts.gstatic.com/s/rajdhani/v9/LDIxapCSOBg7S-QT7q4AOeekWPrP.ttf' },
+    //   fontSize: '35px',
+    //   innerText: 'Enter Text'
+    // },
+    // {
+    //   family: 'Metal Mania',
+    //   files: { regular: 'http://fonts.gstatic.com/s/metalmania/v9/RWmMoKWb4e8kqMfBUdPFJeXCg6UKDXlq.ttf' },
+    //   fontSize: '30px',
+    //   innerText: 'Enter Text'
+    // },
+    // {
+    //   family: 'Piedra',
+    //   files: { regular: 'http://fonts.gstatic.com/s/piedra/v8/ke8kOg8aN0Bn7hTunEyHN_M3gA.ttf' },
+    //   fontSize: '30px',
+    //   innerText: 'Enter Text'
+    // },
+    // {
+    //   family: 'Odibee Sans',
+    //   files: { regular: 'http://fonts.gstatic.com/s/odibeesans/v1/neIPzCSooYAho6WvjeToRYkyepH9qGsf.ttf' },
+    //   fontSize: '30px',
+    //   innerText: 'Enter Text'
+    // },
+    // {
+    //   family: 'Staatliches',
+    //   files: { regular: 'http://fonts.gstatic.com/s/staatliches/v3/HI_OiY8KO6hCsQSoAPmtMbectJG9O9PS.ttf' },
+    //   fontSize: '30px',
+    //   innerText: 'Enter Text'
+    // },
+    // {
+    //   family: 'Lexend Tera',
+    //   files: { regular: 'http://fonts.gstatic.com/s/lexendtera/v1/RrQUbo98_jt_IXnBPwCWtZhARYMgGtWA.ttf' },
+    //   fontSize: '30px',
+    //   innerText: 'Enter Text'
+    // }
   ];
 
   availableFonts = [];
@@ -73,21 +85,24 @@ export class FontFamilyService {
     });
 
     // This add few fonts on top of the list
-    // this.loadCustomFonts();
+    this.loadCustomFonts();
   }
 
   loadCustomFonts() {
-    this.processFonts(this.customTextFonts);
+    this.processFonts(this.customTextFonts, true);
   }
 
-  processFonts(fonts: Array<any>) {
+  processFonts(fonts: Array<any>, customFonts = false) {
     const len = fonts.length;
     for (let i = 0; i < len && i < this.MAX_FONT; i++) {
       const item = {
         label: fonts[i].family,
         value: fonts[i].family
       };
-      this.availableFonts.push(item);
+
+      if (!customFonts || !this.availableFonts.some(t => t.label === fonts[i].family)) {
+        this.availableFonts.push(item);
+      }
 
       this.createFontFamily(item.label, fonts[i].files.regular);
     }
