f09168ca93762705a95358a9c09b6c403b6997dd
Prabhat Ojha <prabhatojha90@gmail.com>
2020-07-13 19:32:16 +0530

    Refactored code

--- frontend/src/app/modules/builder/canvas/select-element/select-element.component.ts
+++ frontend/src/app/modules/builder/canvas/select-element/select-element.component.ts
@@ -20,22 +20,11 @@ export class SelectElementComponent implements OnChanges, OnDestroy {
   @Input() selectedCanvasElement: CanvasElement;
   @Input() container: any;
 
-  @ViewChild('handleRef') handleRef: ElementRef;
   @ViewChild('moveable', { static: false }) moveable: any;
 
-  zIndex: any;
-
   resizeObserver = new ResizeObserver((entries: any) => {
-    console.log('Resize Observer');
-    const rect = entries[0].contentRect;
-    this.styles = Object.assign(this.styles, { height: rect.height + 'px' });
-    this.cd.detectChanges();
+    this.moveable.updateRect();
   });
-
-  styles: any = {};
-  subsribedListeners = {
-    MOUSE_DOWN: 'mousedown'
-  };
 
   previousSelectedNode: any;
 
@@ -48,79 +37,29 @@ export class SelectElementComponent implements OnChanges, OnDestroy {
   }
 
   init() {
-    // this.setZIndex();
-    // this.unbindPreviousItem();
+    this.unbindPreviousItem();
     this.setInitialSize();
   }
 
-  // setZIndex() {
-  //   this.zIndex = this.selectedNode.style[CSS_PROPERTIES.Z_INDEX] || 0;
-  // }
-
-  // unbindPreviousItem() {
-  //   if (this.previousSelectedNode) {
-  //     this.resizeObserver.unobserve(this.previousSelectedNode);
-  //   }
-
-  //   this.previousSelectedNode = this.selectedNode;
-  // }
-
-  setInitialSize() {
-    // const { top, left, width } = this.selectedNode.style;
-    // this.styles = { top, left, width, height: this.selectedNode.offsetHeight + 'px' };
-
-    // console.log('Set Initial Size', this.styles);
-    // if (this.selectedCanvasElement.type === ELEMENT_TYPES.TEXT) {
-    //   this.resizeObserver.observe(this.selectedNode);
-    // }
-
-    if (this.moveable) {
-      console.log('Updating Rect');
-      // this.moveable.updateRect();
+  unbindPreviousItem() {
+    if (this.previousSelectedNode) {
+      this.resizeObserver.unobserve(this.previousSelectedNode);
     }
+
+    this.previousSelectedNode = this.selectedNode;
   }
 
-  // resizing(e) {
-  //   console.log('Resizing', this.getHanlerOffsets());
-  //   CanvasUtils.applyCss(this.selectedNode, this.selectedCanvasElement, this.getHanlerOffsets());
-  // }
-
-  // getHanlerOffsets() {
-  //   const el = this.handleRef.nativeElement;
-  //   const left = el.offsetLeft;
-  //   const top = el.offsetTop;
-  //   const width = el.offsetWidth;
-  //   const height = el.offsetHeight;
-  //   return { left, top, width, height };
-  // }
-
-  // resizingStop(e) {
-
-  // }
-
-  // onDragging(e) {
-  //   console.log('Dragging', e);
-  //   CanvasUtils.applyCss(this.selectedNode, this.selectedCanvasElement, { left: e.x, top: e.y });
-  // }
-
-  // getResizeStyle(e) {
-  //   let { left, top } = e.position;
-  //   const { width, height } = e.size;
-
-  //   left = this.selectedNode[CSS_ELEMENT_PROPS.offsetLeft] + left;
-  //   top = this.selectedNode[CSS_ELEMENT_PROPS.offsetTop] + top;
-
-  //   return { top, left, width, height };
-  // }
-
-  // New Implementation start
+  setInitialSize() {
+    if (this.selectedCanvasElement.type === ELEMENT_TYPES.TEXT) {
+      this.resizeObserver.observe(this.selectedNode);
+    }
+  }
 
   onResize(e) {
     this.selectedNode.style.width = e.width + 'px';
     this.selectedNode.style.height = e.height + 'px';
 
     console.log('Reszing', e);
-    // console.log('Reszing - Rect', this.moveable.getRect());
   }
 
   dragging(e) {
