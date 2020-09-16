8df7dfe7fee5c80b95defcae46b955818a174eeb
Prabhat Ojha <prabhatojha90@gmail.com>
2020-09-16 20:05:15 +0530

    Created HTML part for download popup

--- frontend/src/app/modules/builder/download-canvas/download-canvas.component.ts
+++ frontend/src/app/modules/builder/download-canvas/download-canvas.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+export enum DownloadType {
+  PDF,
+  PNG
+}
 @Component({
   selector: 'app-download-canvas',
   templateUrl: './download-canvas.component.html',
@@ -7,6 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DownloadCanvasComponent implements OnInit {
 
+  DownloadTypes = DownloadType;
+  activeType: DownloadType = DownloadType.PNG;
+  downloadOptions = [
+    {
+      icon: 'png',
+      type: DownloadType.PNG,
+      label: 'PNG'
+    },
+    {
+      icon: 'pdf',
+      type: DownloadType.PDF,
+      label: 'PDF'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
