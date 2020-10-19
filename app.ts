59f8dd345567463a8c30e19f64e3d12c2fba284c
Prabhat Ojha <prabhatojha90@gmail.com>
2020-10-19 10:22:28 +0530

    Updated build config

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 // const mongoose=require('mongoose');
-const bodyparser=require('body-parser');
+const bodyparser = require('body-parser');
 
 var app = express();
 // mongoose.connect('mongodb://localhost/First');
@@ -26,21 +26,33 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// app.get('/*', (req, res) => {
+//   res.sendFile(__dirname + 'build/public/index.html');
+// });
+
+
+app.use(express.static(path.join(__dirname, 'build/public'), {
+  maxAge: '31556952000', // uses milliseconds per docs
+  etag: true  // Will enable cache for static content, by default is also true
+}));
 
 // Add all the routes here
 app.use('/api/projects', indexRouter);
 app.use('/api/images', imageRouter);
 app.use('/api/fonts', fontRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req: any, res: any, next: (arg0: any) => void) {
-  next(createError(404));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'build/public/index.html'));
 });
 
+// catch 404 and forward to error handler
+// app.use((req, res) => {
+//   res.sendFile(__dirname + 'build/public/index.html');
+// });
+
 // error handler
-app.use(function(err: { message: any; status: any; }, req: { app: { get: (arg0: string) => string; }; }, res: { locals: { message: any; error: any; }; status: (arg0: any) => void; render: (arg0: string) => void; }, next: any) {
+app.use(function (err: { message: any; status: any; }, req: { app: { get: (arg0: string) => string; }; }, res: { locals: { message: any; error: any; }; status: (arg0: any) => void; render: (arg0: string) => void; }, next: any) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
